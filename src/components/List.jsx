ccf4b2510158ad0c4056fd68bd30ec6f1bc421e0
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-30 14:47:39 +0900

    design : 리스트 컴포넌트 디자인 완료

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,15 @@
 import "./List.css";
+import TodoItem from "./TodoItem";
 const List = () => {
   return (
     <div className="List">
       <h4>Todo List 💔💔</h4>
       <input placeholder="검색어를 입력하세요."></input>
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
   );
 };
