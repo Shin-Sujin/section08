de014f68f605cb459befef160a3cfc07e6eefffe
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-30 14:32:41 +0900

    design : 시스트 컴포넌트 서치바 디자인

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,11 @@
+import "./List.css";
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="List">
+      <h4>Todo List 💔💔</h4>
+      <input placeholder="검색어를 입력하세요."></input>
+    </div>
+  );
 };
 
 export default List;
