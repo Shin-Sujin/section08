bf3f982d7d014d5a8382e94d8db9538daf6205ac
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-30 14:59:03 +0900

    feat : todos state 생성, mockData로 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,32 @@ import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 import "./App.css";
+import { useState } from "react";
+
+const mockDate = [
+  {
+    id: 0,
+    isDone: false,
+    content: "react 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "산책하기 ",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockDate);
+
   return (
     <div className="App">
       <Header />
