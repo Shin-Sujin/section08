f20ee940b51bc095c2e00ca67e176c0b2efb0112
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-30 14:08:24 +0900

    design : 간격, flex 조정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
-
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
 import "./App.css";
 
 function App() {
-  return <>Todo list</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
