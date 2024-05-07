bd6f901e66c2b8352c1eee8eaf26e2bc689233a9
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-05-07 11:55:31 +0900

    feat : todolist 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import "./App.css";
+import { useRef, useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import "./App.css";
-import { useState, useRef } from "react";
 
-const mockDate = [
+const mockData = [
   {
     id: 0,
     isDone: false,
-    content: "react 공부하기",
+    content: "React 공부하기",
     date: new Date().getTime(),
   },
   {
@@ -20,13 +20,13 @@ const mockDate = [
   {
     id: 2,
     isDone: false,
-    content: "산책하기 ",
+    content: "노래 연습하기",
     date: new Date().getTime(),
   },
 ];
 
 function App() {
-  const [todos, setTodos] = useState(mockDate);
+  const [todos, setTodos] = useState(mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
@@ -36,14 +36,38 @@ function App() {
       content: content,
       date: new Date().getTime(),
     };
+
     setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId
+          ? { ...todo, isDone: !todo.isDone }
+          : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    setTodos(todos.filter((todo) => todo.id !== targetId));
   };
 
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List />
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
     </div>
   );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,14 +1,12 @@
+import { useRef, useState } from "react";
 import "./Editor.css";
-import { useState, useRef } from "react";
+
 const Editor = ({ onCreate }) => {
-  const contentRef = useRef();
-  const onSubmit = () => {
-    if (content === "") {
-      contentRef.current.focus();
-      return;
-    }
-    onCreate(content);
-    setContent("");
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
   };
 
   const onKeydown = (e) => {
@@ -16,22 +14,26 @@ const Editor = ({ onCreate }) => {
       onSubmit();
     }
   };
-  const [content, setContent] = useState("");
 
-  const onChangeContent = (e) => {
-    setContent(e.target.value);
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
   };
 
   return (
     <div className="Editor">
       <input
-        ref={contentRef}
+        ref={inputRef}
         value={content}
         onChange={onChangeContent}
-        placeholder="새로운 todo... "
         onKeyDown={onKeydown}
+        placeholder="새로운 Todo..."
       />
-      <button onClick={onSubmit}>추가 </button>
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
